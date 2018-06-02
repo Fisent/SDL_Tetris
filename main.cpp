f6e5ebd69cd7db62773eff8f5624862da636ce3a
fisent <lukzieniewicz@gmail.com>
2018-06-02 13:44:32 +0200

    Added simple move

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <algorithm>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -14,15 +15,35 @@ int main(int arg, char** args){
 
 	SDL_Event e;
 
+	SDL_Rect* rect = nullptr;
+
+	int x_pos = 0;
+	int y_pos = 0;
+	int speed = 10;
+	
+
 	while(run){
 
 	    while(SDL_PollEvent(&e) != 0){
 	        if(e.type == SDL_QUIT){
 	            run = false;
+	        } else if(e.type == SDL_KEYDOWN){
+	            auto sym = e.key.keysym.sym;
 	        }
 	    }
 
+	    rect = new SDL_Rect();
+        rect->x = x_pos;
+        rect->y = y_pos;
+        rect->w = 10;
+        rect->h = 10;
+
         SDL_FillRect(surface, nullptr, SDL_MapRGB(surface->format, 0x00, 0x00, 0x00));
+        SDL_FillRect(surface, rect, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+        delete rect;
+
+
 
         SDL_UpdateWindowSurface(window);
 	}
